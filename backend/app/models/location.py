525bc4eeda4f732d295236e577b723e9e28f1eb0
Morne Ras <rascustom123@gmail.com>
2026-07-27 22:13:32 +0200

    Added location input to register api

--- backend/app/models/location.py
+++ backend/app/models/location.py
@@ -1,6 +1,6 @@
 # Imports
 # ---
-from sqlalchemy import Float
+from sqlalchemy import Float, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column
 from app.database import Base
 # ---
@@ -13,4 +13,8 @@ class Location(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     latitude: Mapped[float] = mapped_column(Float, nullable=False)
     longitude: Mapped[float] = mapped_column(Float, nullable=False)
+
+    __table_args__ = (
+        UniqueConstraint('latitude', 'longitude', name='uq_lat_lon'),
+    )
 # ---
--- backend/app/services/auth_service.py
+++ backend/app/services/auth_service.py
@@ -2,29 +2,44 @@
 # ---
 from app.models.user import User
 from app.schemas.user import UserCreate
+from app.models.location import Location
+from app.schemas.location import LocationCreate
 from app.schemas.auth import LoginRequest, Token
 from app.database import get_db
 from app.security import hash_password, verify_password, create_access_token
 from sqlalchemy.orm import Session
 from sqlalchemy import select
+from sqlalchemy.dialects.postgresql import insert
 from fastapi import Depends, HTTPException
 # ---
 
 # Register Service
 # ---
-def register(db: Session, user: UserCreate):
-    hashed = hash_password(user.password)
-    new_user = User(
-        username=user.usename,
-        email=user.email,
-        password_hash=hashed
-    )
+def register(db: Session, user: UserCreate, location: LocationCreate):
     try:
+        insert_stmt = insert(Location).values(latitude=location.latitude, longitude=location.longitude)
+
+        upsert_stmt = insert_stmt.on_conflict_do_update(
+            constraint='uq_lat_lon',
+            set_={'latitude': location.latitude}  
+        ).returning(Location.id)
+
+        db.execute(upsert_stmt)
+
+        hashed = hash_password(user.password)
+        new_user = User(
+            username=user.usename,
+            email=user.email,
+            password_hash=hashed
+        )
+
         db.add(new_user)
         db.commit()
     except:
+        db.rollback()
         raise HTTPException(status_code=400, detail="Email already in use")
-    db.refresh(new_user)
+    finally:
+        db.refresh(new_user)
     return new_user
 # ---
 
