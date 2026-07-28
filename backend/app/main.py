b43c6c60db8496ba187b4904b214474af21002de
BroerSakki <besterizak@gmail.com>
2026-07-28 16:54:10 +0200

    Added Developer endpoints

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,9 +3,25 @@
 from fastapi import FastAPI
 from sqlalchemy import text
 from app.database import engine
+# --
+
+# Router Imports
+# ---
 from app.api.auth import router as auth_router
+from app.api.dev.dev import router as dev_database
 # ---
 
 app = FastAPI()
 
+# Root Call
+# ---
+@app.get("/", tags=["Root"])
+def root():
+    return {"message": "Backend is running"}
+# ---
+
+# Routing
+# ---
 app.include_router(auth_router)
+app.include_router(dev_database)
+# ---
