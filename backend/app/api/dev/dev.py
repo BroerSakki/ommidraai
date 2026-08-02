55a3d410c3180b4acb3545a07bc7a7a59d518d1a
Morne Ras <rascustom123@gmail.com>
2026-08-03 00:59:54 +0200

    Added Berlin test data and test api. Chose Berlin since it is quite small. We will later add South Africa

--- backend/app/api/dev/dev.py
+++ backend/app/api/dev/dev.py
@@ -2,8 +2,18 @@
 # ---
 from fastapi import APIRouter
 from app.api.dev.tables import router as table_router
+import httpx
 # ---
 
 router = APIRouter(prefix="/dev")
 
 router.include_router(table_router)
+
+# Run a test with berlin test map data
+@router.get("/test-osrm")
+async def routing_test():
+    url = "http://osrm:5000/route/v1/driving/4.897,52.377;4.904,52.369?steps=true&geometries=geojson"
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url, timeout=10)
+        response.raise_for_status()
+        return response.json()
