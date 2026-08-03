5f978c129a3ec36cd96cff711fa914473d61025c
hog <mgbeukes06@gmail.com>
2026-08-03 12:22:15 +0200

    Now everything for user profile

--- frontend/app/user/profile/page.tsx
+++ frontend/app/user/profile/page.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
-import { ProfileHeader } from "@/app/user/components/Profile/profile-header";
-import { ProfileInfo } from "@/app/user/components/Profile/profile-info";
-import { SavedLocations } from "@/app/user/components/Profile/saved-locations";
+import { ProfileHeader } from "@/app/user/components/profile/profile-header";
+import { ProfileInfo } from "@/app/user/components/profile/profile-info";
+import { SavedLocations } from "@/app/user/components/profile/saved-locations";
 
 export const metadata: Metadata = {
     title: "Profile",
