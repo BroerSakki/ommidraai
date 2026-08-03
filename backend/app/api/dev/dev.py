05be5004b1ffa343021e24163be597e800102998
BroerSakki <besterizak@gmail.com>
2026-08-03 09:07:26 +0200

    Edit lables for osrm test

--- backend/app/api/dev/dev.py
+++ backend/app/api/dev/dev.py
@@ -1,19 +1,19 @@
 # Imports
 # ---
 from fastapi import APIRouter
-from app.api.dev.tables import router as table_router
-import httpx
+from app.api.dev.tables import router as tables_router
+from app.api.dev.osrm import router as osrm_router
 # ---
 
-router = APIRouter(prefix="/dev")
+# Router Setup
+# ---
+router = APIRouter(
+    prefix="/dev"
+)
+# ---
 
-router.include_router(table_router)
-
-# Run a test with berlin test map data
-@router.get("/test-osrm")
-async def routing_test():
-    url = "http://osrm:5000/route/v1/driving/4.897,52.377;4.904,52.369?steps=true&geometries=geojson"
-    async with httpx.AsyncClient() as client:
-        response = await client.get(url, timeout=10)
-        response.raise_for_status()
-        return response.json()
+# Add Routers
+# ---
+router.include_router(tables_router)
+router.include_router(osrm_router)
+# ---
