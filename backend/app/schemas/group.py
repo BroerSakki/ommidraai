d52d1c2dffc4fcaf63676d4ebc0c9a9b4938452d
Morne Ras <rascustom123@gmail.com>
2026-08-14 17:14:58 +0200

    Include group name in get group api

--- backend/app/schemas/group.py
+++ backend/app/schemas/group.py
@@ -1,10 +1,14 @@
 # Imports
 # ---
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 # ---
 
 # Classes
 # ---
 class GroupCreate(BaseModel):
     name: str
+
+    model_config = ConfigDict(
+        from_attributes=True
+    )
 # ---
